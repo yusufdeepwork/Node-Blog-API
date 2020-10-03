b7cd0a4065eaa1b981c4c8f76a3b0c839e49ee64
yusufdeepwork <yusufdeepwork@gmail.com>
2020-10-03 13:45:50 +0300

    Added mongoose module

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+try {
+} catch (error) {
+
+}
+mongoose.connect('mongodb+srv://node-rest-api:'+process.env.MONGO_ATLAS_PW+'@node-rest-api.oskjp.mongodb.net/<dbname>?retryWrites=true&w=majority',
+    {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true},
+    ()=> console.log('DB connected'));
 
 
-app.get('/',(req,res)=> {
-    res.send('hello world,I have started my new project.')
-})
+app.get('/', (req, res)=> {
+  res.send('hello world,I have started my new project.');
+});
 
 
 app.listen('5000');
