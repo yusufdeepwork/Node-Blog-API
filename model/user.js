44bd57bac8ffbb9954dc8f3cc59a4686eda9067e
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-10-06 09:12:33 +0300

    Password Hashing function was added

--- model/user.js
+++ model/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 // eslint-disable-next-line new-cap
 const userSchema = mongoose.Schema({
 
@@ -30,6 +31,24 @@ const userSchema = mongoose.Schema({
   tokenExp: {
     type: Number,
   },
+});
+
+userSchema.pre('save', function(next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, function(err, salt) {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
 });
 
 const User = mongoose.model('User', userSchema);
