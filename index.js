dc5ff9f84263213270346c21026a7e12ecfed1e3
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-10-06 21:47:57 +0300

    Login function is added and implented jsonwebtoken

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 const {User} = require('./model/user');
+const user = require('./model/user');
 
 
 mongoose.connect(config.mongoURI,
@@ -24,6 +25,39 @@ app.post('/api/users/register', (req, res)=>{
     if (err) return res.json({succes: false, err});
     return res.status(200).json({
       succes: true,
+    });
+  });
+});
+
+
+app.get('/api/user/login', (req, res)=>{
+  const {email, password} = req.body;
+
+  User.findOne({email: email}, (err, user)=>{
+    if (!user) {
+      return res.json({
+        loginSuccess: false,
+        message: 'Auth Failed,email not found',
+      });
+    }
+
+    user.comparePassword(password, (err, isMatch)=>{
+      if (!isMatch) {
+        return res.json({loginSuccess: false, message: 'wrog password'});
+      }
+    });
+
+    user.generateToken((err, user)=>{
+      if (err) {
+        return res.status(400).send(err);
+      }
+
+
+      res.cookie('x_auth', user.token)
+          .status(200)
+          .json({
+            loginSuccess: true,
+          });
     });
   });
 });
--- model/user.js
+++ model/user.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
 // eslint-disable-next-line new-cap
 const userSchema = mongoose.Schema({
 
@@ -50,6 +52,27 @@ userSchema.pre('save', function(next) {
     next();
   }
 });
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+  bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+userSchema.methods.generateToken = function(cb) {
+  // eslint-disable-next-line no-var
+  var user = this;
+  // eslint-disable-next-line no-var
+  var token = jwt.sign(user._id.toHexString(), 'secret');
+
+  user.token = token;
+  user.save(function(err, user) {
+    if (err) return cb(err);
+    cb(null, user);
+  });
+};
+
 
 const User = mongoose.model('User', userSchema);
 
